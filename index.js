9c4b0fe7c6ca8d59d63fd44c23e937fc7f1ead67
alexlee94 <alexlee94@protonmail.com>
2022-05-26 00:23:16 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Dice #1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+
+// Dice #2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+// Display winner.
+if (randomNumber1 > randomNumber2) {
+	document.querySelector("h1").innerHTML = "Player 1 Wins!🎖️";
+} else if (randomNumber2 > randomNumber1) {
+	document.querySelector("h1").innerHTML = "🎖️Player 2 Wins!";
+} else {
+	document.querySelector("h1").innerHTML = "Draw!";
+}
